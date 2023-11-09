1e917bb429f207e9fd1b6513bd53a8b5c00dc19d
Jeffrey Washburn <77169258+jswashburn@users.noreply.github.com>
2023-11-09 18:41:09 -0500

    do some project cleanup (#2)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+function DayOfWeek(props) {
+  const [state, setState] = useState({
+    days: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ]
+  });
+
+  useEffect(() => {
+    // setState(state => {
+    //   ...state,
+    //   isToday:
+    // })
+  });
+
+  const day = state.days[props.index];
+
+  return <li className={props.isToday ? "isToday" : ""}>{day}</li>;
+}
+
+export default function App() {
+  const [today, setToday] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload 🌵.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Daily Rosary {new Date().toLocaleDateString()}</h1>
+      <ul>
+        {
+          [...Array(7).keys()].map(i => <DayOfWeek index={i} />)
+        }
+      </ul>
     </div>
   );
 }
-
-export default App;
