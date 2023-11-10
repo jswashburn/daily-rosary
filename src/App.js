96d6e67bfce25b821cdbabc0f34564b4964b1a32
Jeffrey Washburn <jwashburn@microsoft.com>
2023-11-10 14:54:24 -0500

    change to react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./Navbar"
 import Home from "./pages/Home";
 import Monday from "./pages/Monday";
@@ -10,37 +11,20 @@ import Saturday from "./pages/Saturday";
 import Sunday from "./pages/Sunday";
 
 export default function App() {
-  let component;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />
-      break
-    case "/monday":
-      component = <Monday />
-      break
-    case "/tuesday":
-      component = <Tuesday />
-      break
-    case "/wednesday":
-      component = <Wednesday />
-      break
-    case "/thursday":
-      component = <Thursday />
-      break
-    case "/friday":
-      component = <Friday />
-      break
-    case "/saturday":
-      component = <Saturday />
-      break
-    case "/sunday":
-      component = <Sunday />
-      break
-  }
   return (
     <div className="App">
       <Navbar />
-      <div className="content">{component}</div>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/monday" element={<Monday />} />
+          <Route path="/tuesday" element={<Tuesday />} />
+          <Route path="/wednesday" element={<Wednesday />} />
+          <Route path="/thursday" element={<Thursday />} />
+          <Route path="/friday" element={<Friday />} />
+          <Route path="/saturday" element={<Saturday />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return (
         <div className="navbar">
-            <a href="/" className="navbar__site-title">Daily Rosary</a>
+            <Link to="/" className="navbar__site-title">Daily Rosary</Link>
             <ul className="navbar__days-list">
-                <li className="navbar__days-list__day"><a href="/monday" className="navbar__days-list__day__link">Monday - Joyful</a></li>
-                <li className="navbar__days-list__day"><a href="/tuesday" className="navbar__days-list__day__link">Tuesday - Sorrowful</a></li>
-                <li className="navbar__days-list__day"><a href="/wednesday" className="navbar__days-list__day__link">Wednesday - Glorious</a></li>
-                <li className="navbar__days-list__day"><a href="/thursday" className="navbar__days-list__day__link">Thursday - Luminous</a></li>
-                <li className="navbar__days-list__day"><a href="/friday" className="navbar__days-list__day__link">Friday - Sorrowful</a></li>
-                <li className="navbar__days-list__day"><a href="/saturday" className="navbar__days-list__day__link">Saturday - Joyful</a></li>
-                <li className="navbar__days-list__day"><a href="/sunday" className="navbar__days-list__day__link">Sunday - Glorious</a></li>
+                <li className="navbar__days-list__day"><Link to="/monday" className="navbar__days-list__day__link">Monday - Joyful</Link></li>
+                <li className="navbar__days-list__day"><Link to="/tuesday" className="navbar__days-list__day__link">Tuesday - Sorrowful</Link></li>
+                <li className="navbar__days-list__day"><Link to="/wednesday" className="navbar__days-list__day__link">Wednesday - Glorious</Link></li>
+                <li className="navbar__days-list__day"><Link to="/thursday" className="navbar__days-list__day__link">Thursday - Luminous</Link></li>
+                <li className="navbar__days-list__day"><Link to="/friday" className="navbar__days-list__day__link">Friday - Sorrowful</Link></li>
+                <li className="navbar__days-list__day"><Link to="/saturday" className="navbar__days-list__day__link">Saturday - Joyful</Link></li>
+                <li className="navbar__days-list__day"><Link to="/sunday" className="navbar__days-list__day__link">Sunday - Glorious</Link></li>
             </ul>
         </div>
     )
