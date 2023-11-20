b23006ea44f23e6c8e09e22a44c805f827d280ed
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2023-11-20 13:13:36 -0600

    add a basic progress bar

--- src/components/MysteryGroup.js
+++ src/components/MysteryGroup.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { useRef } from "react";
 import ContentSection from "./ContentSection";
 
 export default function MysteryGroup(props) {
-  const rootRef = useRef(null);
   const sectionsRef = useRef(null);
+  const [currSection, setCurrSection] = useState(0);
   const [sections] = useState(
     props.sections.map((section, index) => {
       return (
@@ -13,10 +14,10 @@ export default function MysteryGroup(props) {
             const map = getMap();
             // node refers to the node referred by ContentSection
             if (node) {
-              // observerRef.current.observe(node);
-              map.set(index, node);
+              node.sectionPosition = section.position;
+              map.set(section.position, {node: node});
             } else {
-              map.delete(index);
+              map.delete(section.position);
             }
           }}
           key={index}
@@ -30,7 +31,7 @@ export default function MysteryGroup(props) {
 
   const scrollTo = (position) => {
     const map = getMap();
-    const section = map.get(position);
+    const section = map.get(position).node;
     console.log("Scrolling to " + position);
     section.scrollIntoView({
       behavior: "smooth",
@@ -45,18 +46,59 @@ export default function MysteryGroup(props) {
     return sectionsRef.current;
   };
 
+  useEffect(() => {
+    const map = getMap();    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setCurrSection(entry.target.sectionPosition);
+        }
+      });
+    }, {threshold: 1});
+
+    map.forEach((value, index, map) => {
+      observer.observe(value.node);
+    });
+
+    return () => {
+      map.forEach((value, index, map) => {
+        observer.unobserve(value.node);
+        observer.disconnect();
+      });
+    }
+  }, []);
+
+  const getProgressBarWidth = () => {
+    const p = ((currSection + 1) / (sections.length)) * 100
+    return p + '%'
+  }
+
+  const progressBarStyle = {
+    width: getProgressBarWidth(),
+    height: '2em',
+    backgroundColor: 'blue',
+    position: 'sticky',
+    top: '8em',
+    alignSelf: 'start',
+    transitionProperty: 'width',
+    transition: '150ms'
+  }
+
   return (
-    <div ref={rootRef} className={"content " + props.theme}>
-      <h1>{props.title}</h1>
-      <nav>
-        <button onClick={() => scrollTo(1)}>Skip to Mysteries</button>
-        <button onClick={() => scrollTo(1)}>First Mystery</button>
-        <button onClick={() => scrollTo(2)}>Second Mystery</button>
-        <button onClick={() => scrollTo(3)}>Third Mystery</button>
-        <button onClick={() => scrollTo(4)}>Fourth Mystery</button>
-        <button onClick={() => scrollTo(5)}>Fifth Mystery</button>
-      </nav>
-      {sections}
-    </div>
+    <>
+      <div className={"content " + props.theme}>
+        <div style={progressBarStyle}>{currSection}</div>
+        <h1>{props.title}</h1>
+        {/* <nav>
+          <button onClick={() => scrollTo(1)}>Skip to Mysteries</button>
+          <button onClick={() => scrollTo(1)}>First Mystery</button>
+          <button onClick={() => scrollTo(2)}>Second Mystery</button>
+          <button onClick={() => scrollTo(3)}>Third Mystery</button>
+          <button onClick={() => scrollTo(4)}>Fourth Mystery</button>
+          <button onClick={() => scrollTo(5)}>Fifth Mystery</button>
+        </nav> */}
+        {sections}
+      </div>
+    </>
   );
 }
